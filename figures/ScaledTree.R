189c1a5fa243a2c3551d5382bde0cf638d5123d2
April Wright <wright.aprilm@gmail.com>
2019-08-05 09:38:48 -0500

    initial commit of things to add to github

---
+++ figures/ScaledTree.R
@@ -0,0 +1,10 @@
+library(phytools)
+library(ggtree)
+
+tree <- pbtree(b = 1, d = .5, n=5)
+ggplot(tree, aes(x, y)) + geom_tree() + theme_tree()  + geom_nodepoint(color="grey", alpha=3/4, size=5, shape="triangle") + geom_tippoint(color="grey", alpha=1/2, size=5) 
+
+age_df <- read.csv("ages", sep = "\t", row.names = "taxon")
+scaley <- paleotree::cal3TimePaleoPhy(tree, age_df, sampRate = .0001, brRate=1, extRate = .5)
+
+strap::geoscalePhylo(scaley, age_df, units = c("Epoch", "Era"), tick.scale = 10, cex.ts = .5, cex.age = .5)
