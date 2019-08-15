fd56d514131aed9f76c2d9d1778d80e19948fdff
Rachel Warnock <rachel.warnock@gmail.com>
2019-08-15 19:58:23 +0200

    updates to ms

--- figures/ScaledTree.R
+++ figures/ScaledTree.R
@@ -1,8 +1,11 @@
 library(phytools)
 library(ggtree)
 
+set.seed(124)
+
 tree <- pbtree(b = 1, d = .5, n=5)
-ggplot(tree, aes(x, y)) + geom_tree() + theme_tree()  + geom_nodepoint(color="grey", alpha=3/4, size=5, shape="triangle") + geom_tippoint(color="grey", alpha=1/2, size=5) 
+tree <- pbtree(b = 1, d = .5, n=5)
+ggplot(tree, aes(x, y)) + geom_tree() + theme_tree()  + geom_nodepoint(color="grey", alpha=4/5, size=5, shape="triangle") + geom_tippoint(color="grey", alpha=4/5, size=5) 
 
 age_df <- read.csv("ages", sep = "\t", row.names = "taxon")
 scaley <- paleotree::cal3TimePaleoPhy(tree, age_df, sampRate = .0001, brRate=1, extRate = .5)
